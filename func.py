de4c19acbfbafe15721c230a7d3f09a53bcdd49c
lgbao123 <luugiabao2609@gmail.com>
2022-09-21 16:19:35 +0700

    update global , nonlocal keyword

--- func.py
+++ func.py
@@ -8,3 +8,23 @@ def super_func(*args ,**kwargs):
     return sum(args)
 print(super_func.__doc__) # use .__doc__ to print comment in function 
 print(super_func(1,2,3,4,5, a=10,b=15))
+
+
+# global keyword : Sử dụng để truy cập global 
+# nonlocal keyword : Sử dụng để truy cập biến parent local 
+
+
+def outer():
+    x = 'local'
+    def inner():
+        nonlocal x 
+        x = 'nonlocal'
+        print('inner : ',x)
+    inner()
+    print('outer : ',x)
+outer()
+
+# Scope 
+# 1.start with local 
+# 2.Parent local 
+# 3.Global 
